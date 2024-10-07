501b411322e71651bf28a7f0e7c196da6468a3e9
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2024-10-07 11:06:02 -0700

    Step 1 completed

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button: HTMLButtonElement = document.createElement('button'); 
+button.textContent = "🍆"; 
+document.body.appendChild(button); 
+
