a123e5b33b5bccb3f8f9757e00f97fe7365a3bea
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2024-10-07 11:55:38 -0700

    Step 2 Completed

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,15 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button: HTMLButtonElement = document.createElement('button'); 
-button.textContent = "🍆"; 
-document.body.appendChild(button); 
+const button: HTMLButtonElement = document.createElement("button");
+button.textContent = "🍆";
+document.body.appendChild(button);
 
+let buttonCounter: number = 0; 
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+button.addEventListener('click', (event:MouseEvent) => {
+    buttonCounter++; 
+    console.log("Button clciked: " + buttonCounter+ '\n');
+    button.textContent = ("🍆 clicked " + buttonCounter + " times\n"); 
+}); 
